fec45634a7487bcd14efc6fcf05e8a672b677b34
Stephen Cavaliere <stephencavaliere@gmail.com>
2018-04-20 11:51:02 -0600

    initial commit

---
+++ projects/ngcommunity/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async(() => {
+    TestBed
+      .configureTestingModule({
+        imports: [
+          RouterTestingModule,
+        ],
+        declarations: [
+          AppComponent,
+        ],
+      })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
