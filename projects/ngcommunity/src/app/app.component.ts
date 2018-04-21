508928e3ddf5091a2dc024da5d7ae3ea71ab1345
Stephen Cavaliere <stephencavaliere@gmail.com>
2018-04-20 19:40:51 -0600

    feat: add basic material layout

--- projects/ngcommunity/src/app/app.component.ts
+++ projects/ngcommunity/src/app/app.component.ts
@@ -1,8 +1,16 @@
+import { BreakpointObserver, BreakpointState, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent { }
+export class AppComponent {
+  isHandset: Observable<BreakpointState> = this.breakpointObserver.observe(Breakpoints.Handset);
+
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+  ) {}
+}
--- projects/ngcommunity/src/app/app.module.ts
+++ projects/ngcommunity/src/app/app.module.ts
@@ -1,5 +1,18 @@
+import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatSidenavModule,
+  MatTabsModule,
+  MatToolbarModule,
+} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +20,19 @@ import { AppComponent } from './app.component';
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatDividerModule,
+    MatTabsModule,
   ],
   declarations: [
     AppComponent,
